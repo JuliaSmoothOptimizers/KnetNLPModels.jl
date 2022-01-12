70c301a93cb313f7e8635973c865ac5e7ea12fba
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-01-12 14:18:54 +0100

    correctif

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,7 +36,7 @@ xtst,ytst = MNIST.testdata(Float32);  ytst[ytst.==0] .= 10
 	(c::Chainnll)(d::Knet.Data) = Knet.nll(c; data=d, average=true)
 	
 	# La base de donnée, base de test
-	dtrn = minibatch($xtrn, ytrn, 100; xsize=(size(xtrn,1),size(xtrn,2),1,:))
+	dtrn = minibatch(xtrn, ytrn, 100; xsize=(size(xtrn,1),size(xtrn,2),1,:))
 	dtst = minibatch(xtst, ytst, 100; xsize=(size(xtst,1),size(xtst,2),1,:))
 	
 	LeNet = Chainnll(Conv(5,5,1,20), Conv(5,5,20,50), Dense(800,500), Dense(500,10,identity))
