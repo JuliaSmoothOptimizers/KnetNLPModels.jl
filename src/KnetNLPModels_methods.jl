07b4a5d62ed452c2afb8c742a8568887793e5384
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-01-24 11:41:09 +0100

    .zenodo + fix definition des fonctions

--- src/KnetNLPModels_methods.jl
+++ src/KnetNLPModels_methods.jl
@@ -12,7 +12,7 @@ Then it sets these variables to the nested array.
 set_vars!(vars, new_w :: Vector) = map(i -> vars[i].value .= new_w[i], 1:length(vars))
 set_vars!(vars, new_w :: Vector{KnetArray}) = map(i -> vars[i].value .= new_w[i], 1:length(vars))
 set_vars!(chain::T, new_w :: Vector) where T <: Chain = set_vars!(params(chain), build_nested_array_from_vec!(chain, new_w) ) 
-function set_vars!(model::KnetNLPModel, new_w :: Vector) 
+function set_vars!(model::KnetNLPModel{T,S,C}, new_w :: Vector) where {T,S,C}
   param = build_nested_array_from_vec!(model, new_w)	
   set_vars!(param, model.nested_knet_array)
   model.w .= new_w
@@ -25,8 +25,8 @@ Build a vector of KnetArrays from v similar to Knet.params(model.chain).
 Call build_array iteratively to build each intermediary KnetArrays.
 This methods is not optimised, it consumes memory.
 """
-build_nested_array_from_vec(model::KnetNLPModel, v::Vector{T}) where T <: Number = begin build_nested_array_from_vec!(model, v); model.nested_knet_array end 
-function build_nested_array_from_vec!(model::KnetNLPModel, v::Vector{T}) where T <: Number
+build_nested_array_from_vec(model::KnetNLPModel{T,S,C}, v::Vector{T}) where {T,S,C} = begin build_nested_array_from_vec!(model, v); model.nested_knet_array end 
+function build_nested_array_from_vec!(model::KnetNLPModel{T,S,C}, v::Vector{T}) where {T,S,C}
   param = params(model.chain)
   size_param = mapreduce((var_layer -> reduce(*,size(var_layer))), +, param)
   flatten_params = vcat_arrays_vector(param)
@@ -48,7 +48,7 @@ end
 
 Evaluate ``f(x)``, the objective function of `nlp` at `x`.
 """
-function NLPModels.obj(nlp :: KnetNLPModel{T,S,C}, w :: AbstractVector{T}) where T <: Number
+function NLPModels.obj(nlp :: KnetNLPModel{T,S,C}, w :: AbstractVector{T}) where {T,S,C}
   w != nlp.w && set_vars!(nlp, w)
 	increment!(nlp, :neval_obj)  
   f_w = nlp.chain(nlp.minbatch_train)
@@ -60,7 +60,7 @@ end
 
 Evaluate ``∇f(x)``, the gradient of the objective function at `x` in place.
 """
-function NLPModels.grad!(nlp :: KnetNLPModel{T,S,C}, w :: AbstractVector{T}, g :: AbstractVector{T}) where T <: Number
+function NLPModels.grad!(nlp :: KnetNLPModel{T,S,C}, w :: AbstractVector{T}, g :: AbstractVector{T}) where {T,S,C}
   length(w) != length(g) && error("different size vector: w, g: NLPModels.grad!")
   increment!(nlp, :neval_grad)
   w != nlp.w && set_vars!(nlp, w)
@@ -77,14 +77,14 @@ end
 
 Take a new minibatch for the KnetNLPModel. Usually use before a new evaluation.
 """
-reset_minibatch_train!(nlp :: KnetNLPModel) = nlp.minbatch_train = create_minibatch(nlp.data_train[1], nlp.data_train[2], nlp.size_minibatch)
+reset_minibatch_train!(nlp :: KnetNLPModel{T,S,C}) where {T,S,C} = nlp.minbatch_train = create_minibatch(nlp.data_train[1], nlp.data_train[2], nlp.size_minibatch)
 
 """
     reset_minibatch_test!(nlp)	
 
 Take a new minibatch for the KnetNLPModel. Usually use before a new accuracy test.
 """
-reset_minibatch_test!(nlp :: KnetNLPModel) = nlp.minbatch_test = create_minibatch(nlp.data_test[1], nlp.data_test[2], nlp.size_minibatch)
+reset_minibatch_test!(nlp :: KnetNLPModel{T,S,C}) where {T,S,C} = nlp.minbatch_test = create_minibatch(nlp.data_test[1], nlp.data_test[2], nlp.size_minibatch)
 
 """ 
     accuracy(nlp)
