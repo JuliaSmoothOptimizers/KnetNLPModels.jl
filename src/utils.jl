e8ade68b90932c931fb5065a0ca5a6a2ea7917b1
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-06-21 16:48:48 -0400

    adding CPU support

--- src/utils.jl
+++ src/utils.jl
@@ -86,6 +86,12 @@ function build_layer_from_vec!(myArray ::Array{T, N} where N, v :: Vector{T}, in
   return sizeArray
 end
 
+function build_layer_from_vec!(myArray ::Array{T, N} where N, v :: Vector{T}, index :: Int) where {T <: Number}
+	sizeArray = reduce(*, size(myArray))
+	copyto!(myArray, v[index+1:index+sizeArray])
+  return sizeArray
+end 
+
 """
     build_layer_from_vec!(array, v :: Vector{T}, index :: Int) where {T <: Number}
 
